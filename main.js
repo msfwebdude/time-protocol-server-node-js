38a41fb887b4b2dabde7caedee2c0993dd2819be
Mike Firoved <msfwebdude@gmail.com>
2019-08-26 16:09:23 -0700

    added TCP/IP support

--- main.js
+++ main.js
@@ -15,8 +15,12 @@
     // Relax, problem solved
     dns.setServers(["8.8.8.8", "8.8.4.4"]);     
 
+    // #####################
+    // #    UDP Section    #
+    // #####################
+
     var udpMessageReceived = function(msg, rinfo){
-        var tvl = getTimeValue();
+        var tvl = getTimeValue(2);
         var msg = new Uint8Array(tvl.split(""));
         // prevent volley of udp datagrams bouncing back and forth, time responses are non zero length
         if(msg.length < 1){
@@ -43,12 +47,65 @@
     udpSocket.bind(37, "localhost", udpListeningOnBind);
 
 
-    function getTimeValue(){
+    // #####################
+    // #    TCP SECTION    #
+    // #####################
+    var server = new tcp.Server();
+
+    var startTcpServer = () => {
+        server.close(
+            () => {
+                server.listen(
+                    {
+                        port: 37,
+                        host: 'localhost',
+                    }
+                );
+            }
+        );
+    };
+    server.on('connection',
+        (sock) => {
+            var tvl = getTimeValue(10);
+            console.log('connected from', sock.address());
+            sock.write(
+                tvl, 
+                'utf8',
+                () => {
+                    sock.end();
+                }
+            );
+        }
+    );
+    server.on('listening', 
+        () => {
+            console.log('TCP/IP Listening on', server.address());
+        }
+    )
+    server.on('error', 
+        (err) => {
+            console.log('Error:', err);
+            startTcpServer();
+        }
+    );
+
+    startTcpServer();
+
+
+
+
+
+    // ####################
+    // # HELPER FUNCTIONS #
+    // ####################
+
+
+    function getTimeValue(r){
         var beginDate = new Date("1900-01-01T00:00:00.000Z");
         var todayDate = new Date();
         var deviation = Math.round(((todayDate - beginDate) / 1000))
 
-        return deviation.toString(2);
+        return deviation.toString(r);
     }
 
     // if not using NPM, get version manually
